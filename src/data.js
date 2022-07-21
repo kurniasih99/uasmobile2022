df2a3e6d110b91d041533d51b54da122bd09edd1
kurniasih99 <knias2020@gmail.com>
2022-07-21 14:16:12 +0700

    project uas

---
+++ src/data.js
@@ -0,0 +1,124 @@
+const books = 
+[
+	{
+		id: "1",
+		"title": "Magic Warmer Plus Miyako MCM-612",
+		"description": "Penanak nasi yang memiliki fungsi 3 in 1 yaitu sebagai pemasak nasi, mengukus, dan menghangatkan makanan.",
+		"subtitle": "Hasanudin Abdurakhman",
+		"jmlHal": 288,
+		"categorie":"Motivation & Self-Help",
+		"penerbit":"Kepustakaan Populer Gramedia",
+		"picture": "https://www.miyako.co.id/wp-content/uploads/2018/10/MIYAKO_MAGIC-WARMER-PLUS-MCM-612-01-scaled.jpg",
+		"price":207000
+	},
+	{
+		id: "2",
+		"title": "Electric Fan Miyako KAS-1618 KB",
+		"subtitle": "",
+		"description": "Kipas angin KAS-1618 KB merupakan kipas angin dengan 2 fungsi yaitu kipas angin meja (desk fan) dan kipas angin berdiri di lantai (stand fan).",
+		"jmlHal": 184,
+		"categorie":"Reference & Dictionary",
+		"penerbit":"Bhuana Ilmu Populer",
+		"picture": "https://www.miyako.co.id/wp-content/uploads/2018/10/KAS-1618-KB-01-scaled.jpg",
+		"price":225000
+	},
+	{
+		id: "3",
+		"title": "Miyako WD-186 H",
+		"subtitle": "Apa jadinya kalau traveler yang sudah menghabiskan ribuan jam di atas pesawat ternyata menyimpan ketakutan terhadap transportasi udara, seorang polisi yang menemukan bahwa tempat wisata favoritnya adalah lokasi populer untuk bunuh diri, atau traveler yang membayar mahal demi kenyamanan tapi terpaksa tertahan belasan jam di atas pikap pengangkut sapi? Setiap traveler pasti memiliki tantangannya tersendiri dalam menaklukan setiap medan. Jalan terjal yang harus dilalui memang tak pernah sama. Tapi mereka memiliki tekad yang serupa: harus sampai ke tempat tujuan. Pesan itulah yang ingin disampaikan oleh ke-15 traveler dalam buku ini. Latar belakang mereka yang begitu beragam, mulai dari wartawan, description novel, peneliti, hingga polisi membuat cerita-cerita yang dituturkan menjadi penuh warna.",
+		"description": "Miyako WD-190 H merupakan dispenser portable yang memiliki desain kompak dan fleksible untuk diletakkan dimana saja.",
+		"jmlHal": 237,
+		"categorie":"Travel",
+		"penerbit":"MIZAN",
+		"picture": "https://images.tokopedia.net/img/cache/500-square/product-1/2017/12/2/1687614/1687614_0640e85f-0a8c-476d-8169-0678d219e881_850_850.jpg",
+		"price":155000
+	},
+	{
+		id: "4",
+		"title": "Setrika Miyako EI-1009 M NP",
+		"subtitle": "Warren Buffett, investor legendaris dan salah satu orang terkaya di dunia, dalam seminarnya di tahun 1984 di kampus Columbia Business School, Amerika Serikat, memaparkan tentang kinerja investasi dari tujuh investor saham penganut metode value investing, termasuk dirinya sendiri, yang kesemuanya sukses menghasilkan persentase keuntungan yang secara signifikan di atas rata-rata pasar. Menurut Buffett, ketujuh investor tersebut memiliki gaya investasinya masing-masing yang berbeda satu sama lain, termasuk pilihan sahamnya juga berbeda-beda. Tapi pertanyaannya kemudian, bagaimana kita tahu sebuah saham itu bagus atau jelek? Dan bagaimana caranya untuk menilai apakah harganya mahal, wajar, atau murah? Nah, di buku Value Investing: Beat the Market in Five Minutes! ini, kita akan menjawab semua pertanyaan tersebut secara lugas dan lengkap namun mudah untuk dipahami, berdasarkan pengalaman description sebagai value investor itu sendiri. Yup, jadi buku ini tidak sekadar memaparkan teori tentang value investing, melainkan juga contoh-contoh penerapannya dalam berinvestasi di saham. Fakta menariknya adalah, ketika nanti Anda sudah menguasai dan mampu mempraktikkan metode value investing ini dengan baik, maka anda hanya akan perlu bekerja kurang dari 5 menit saja setiap harinya, tapi hasil profi tnya tetap akan luar biasa!",
+		"description": "Setrika listrik miyako cocok untuk menyetrika bahan jeans sekasar apapun tanpa takut tergores, lebih licin, anti lengket dan lebih tahan gores.",
+		"jmlHal": 258,
+		"categorie":"Business & Investing",
+		"penerbit":"Elex Media Komputindo",
+		"picture": "https://www.miyako.co.id/wp-content/uploads/2018/10/1b-1.jpg",
+		"price":120000
+	},
+	{
+		id: "5",
+		"title": "Miyako Blender BL102 GS",
+		"subtitle": "Dia bagai malaikat bagi keluarga kami. Merengkuh aku, adikku, dan Ibu dari kehidupan jalanan yang miskin dan nestapa. Memberikan makan, tempat berteduh, sekolah, dan janji masa depan yang lebih baik. Dia sungguh bagai malaikat bagi keluarga kami. Memberikan kasih sayang, perhatian, dan teladan tanpa mengharap budi sekali pun. Dan lihatlah, aku membalas itu semua dengan membiarkan mekar perasaan ini. Ibu benar, tak layak aku mencintai malaikat keluarga kami. Tak pantas. Maafkan aku, Ibu. Perasaan kagum, terpesona, atau entahlah itu muncul tak tertahankan bahkan sejak rambutku masih dikepang dua. Sekarang, ketika aku tahu dia boleh jadi tidak pernah menganggapku lebih dari seorang adik yang tidak tahu diri, biarlah... Biarlah aku luruh ke bumi seperti sehelai daun… daun yang tidak pernah membenci angin meski harus terenggutkan dari tangkai pohonnya.",
+		"description": "Blender Miyako dapat memudahkan Anda untuk menyiapkan aneka jus segar dan bumbu masakan.",
+		"jmlHal": 268,
+		"categorie":"Teen & Young Adult Fiction",
+		"penerbit":"Gramedia Pustaka Utama",
+		"picture": "http://static.bmdstatic.com/pk/product/large/5aea8aee4b2d7.jpg",
+		"price":323000
+	},
+	{
+		id: "6",
+		"title": "Mixer Miyako SM-625",
+		"subtitle": "“Bicara jodoh adalah bicara tentang hal yang jauh ke depan: bukan semata-mata baiknya fisik lantas bisa membuat Anda tertarik. Saya yakin setiap orang sudah tertulis jodohnya. Sejauh apa pun takdir dunia memisahkan, pasti ada cara Tuhan untuk bisa mempersatukan. Buku Jodoh Pasti Bertemu karya Arif Hidayat ini sangat mencerahkan. Ditulis dengan bahasa yang santun, namun sangat membangun. Begitu menginspirasi, tapi tidak menggurui.”",
+		"description": "Merupakan alat pengaduk yang bisa Anda gunakan untuk mempermudah proses pencampuran makanan sehingga menjadikan proses memasak ataupun membuat kue menjadi lebih mudah dan menyenangkan.",
+		"jmlHal": 180,
+		"categorie":"Religion & Spirituality",
+		"penerbit":"Grasindo",
+		"picture": "https://www.miyako.co.id/wp-content/uploads/2018/10/SM-625-01.jpg",
+		"price":276000
+	},
+	{
+		id: "7",
+		"title": "HairDryer Miyako HD-550 G",
+		"subtitle": "Peran teknologi semakin lama semakin berkembang pesat dari hari ke harinya. Jika Anda perhatikan, secara berkala, beberapa programmer hebat membuat aplikasi baru, membangun jaringan, menciptakan software terbaru hampir setiap hari. Keadaan ini membuat kita mau-tidak-mau harus memiliki pengetahuan yang tidak sedikit dengan dunia Internet. Baik itu untuk Software ataupun Hardware. Namun tahukan Anda, setiap aplikasi atau jaringan mempunyai celah untuk ditembus? Tidak ada sistem yang aman. Itu adalah semboyan yang ditanamkan untuk para hacker yang berkecimpung di dunia maya. Beberapa cara dapat dicoba untuk membobol sistem. Seperti binary pada Android, rooting pada cpanel website atau menanamkan shell ke dalamnya, menjadi mata-mata pada jaringan orang lain, mengubah sistem dasar Windows, membuat virus yang bisa merusak sistem sekalipun bisa Anda lakukan.",
+		"description": "Memiliki 1 pilihan kecepatan dan kekuatan panas yang bisa Anda sesuaikan dengan kebutuh, membuat rambut lebih cepat kering dan sempurna.",
+		"jmlHal": 128,
+		"categorie":"Computers & Technology",
+		"penerbit":"Elex Media Komputindo",
+		"picture": "https://id-live-01.slatic.net/p/54d56c320636def40338d4fb0e411ddf.jpg",
+		"price":130000
+	},
+	{
+		id: "8",
+		"title": "Vacuum Cleaner Miyako",
+		"subtitle": "Salah satu sumber pendapatan yang bisa kamu andalkan di internet adalah Google AdSense. Jika kamu ingin menghasilkan pendapatan sebagai pemilik web ataupun pemilik saluran Youtube, kamu bisa gunakan Google AdSense. Lantas, bagaimana cara dan langkah demi langkah menghasilkan pendapatan dari Google AdSense? Temukan jawaban dan panduan lengkapnya di buku ini. Inilah pembahasan menarik yang ada di buku ini: ● Pengenalan tentang Google AdSense. Mulai dari apa itu Google AdSense, bagaimana cara kerjanya, hingga hal-hal yang perlu disiapkan. ● Panduan lengkap membuat blog/web secara langkah demi langkah. Jika kamu belum punya web sekalipun, kamu akan dipandu mulai dari awal untuk membuat blog/web. ● Panduan dan cara mendaftar Google AdSense. ● Panduan dan cara menampilkan iklan Google AdSense di blog. ● Berbagai tools keren untuk membantumu menjadi blogger dan YouTuber. ● Panduan membuat saluran YouTube yang menarik dan bisa dimonetisasi dengan Google AdSense. Cara menampilkan iklan AdSense di saluran YouTube-mu. ● Panduan langkah demi langkah menjadi seorang YouTuber dan pemilik saluran YouTube. Selain itu, sebagai pembaca buku ini kamu juga akan mendapatkan belasan bonus video tutorial yang akan memandumu belajar seputar blog, YouTube, dan Google AdSense. Keunggulan buku ini adalah kamu tidak hanya dipandu untuk membuat web/blog, tetapi kamu juga dipandu membangun passive income melalui saluran YouTube. Jadi, kamu bisa sekaligus belajar menjadi blogger dan YouTuber.",
+		"description": "Menjaga kebersihan rumah merupakan hal penting. Dengan menggunakan Vacuum Cleaner dari Miyako akan membantu Anda untuk memberishkan rumah dari debu dan kotoran membandel.",
+		"jmlHal": 248,
+		"categorie":"Computers & technology",
+		"penerbit":"Elex Media Komputindo",
+		"picture": "https://images.tokopedia.net/img/cache/500-square/product-1/2020/1/15/135783016/135783016_b8584146-c4d1-4c30-a1f6-a2d11ff0bab5_822_822.jpg",
+		"price":1286000
+	},
+	{
+		id: "9",
+		"title": "Cokeware Miyako",
+		"subtitle": "PHP merupakan pemrograman wajib bagi para web developer. Buku ini menjelaskan PHP sejak dari dasar hingga pemrograman web berbasis database. Buku ini menyediakan hampir sebagian besar teori yang Anda butuhkan untuk mengenal PHP dan segala kemampuannya. Dengan demikian, Anda bisa mengenali PHP secara cepat dan langsung mempraktikkannya untuk membuat aplikasi website.",
+		"description": " Memasak jadi lebih nyaman, aman dan cepat dengan pilihan cookware dari Miyako",
+		"jmlHal": 274,
+		"categorie":"Computers & Technology",
+		"penerbit":"Elex Media Komputindo",
+		"picture": "https://cf.shopee.co.id/file/f345feea01c7d0ee48d6a65d04867033",
+		"price":90000
+	},
+	{
+		id: "10",
+		"title": "Electric Oven Miyako",
+		"subtitle": "Sekarang ini membuat aplikasi Android tidak harus menguasai Java. Kita dapat dengan mudah membuat aplikasi Android dengan menggunakan bahasa pemrograman web, yaitu HTML, Javascript, dan CSS. Salah satu framework yang paling terkenal dan banyak digunakan untuk membuat aplikasi Android dengan bahasa pemrograman web adalah Ionic. Ionic berkolaborasi dengan Cordova sehingga memungkinkan aplikasi yang dibuat dapat mengakses fitur Native seperti kamera, kontak, geolocation, dan sebagainya. Pembahasan dalam buku ini dimulai dari dasar Typescript, Angular, dan Ionic. Selanjutnya dibahas berbagai komponen Ionic, setting Ionic untuk Android, hingga penggunaan native Ionic. Dibahas juga penyimpanan data pada Ionic termasuk integrasi Ionic dengan PHP dan MySQLi disertai dengan contoh operasi CRUD. Pada bagian akhir dibahas langkah demi langkah membuat aplikasi berbagi foto ala Instagram. Pembahasan selengkapnya mencakup: - Dasar-dasar Typescript - Dasar-dasar Angular - Dasar-dasar Ionic - Komponen Ionic - Setting Platform Android - Plugin Native Ionic - Integrasi Ionic dengan PHP dan MySQLi - Tips dan Trik Ionic - Membuat aplikasi berbagi foto ala Instagram.",
+		"description": "Pilihan Oven Toaster Miyako dengan kapasitas memanggang hingga 6 liter dengan balutan desain simple dan cantik. Selain membantu pekerjaan Anda di dapur, Oven Toaster ini juga akan mempercantik dapur Anda.",
+		"jmlHal": 258,
+		"categorie":"Computers & Technology",
+		"penerbit":"Elex Media Komputindo",
+		"picture": "https://id-test-11.slatic.net/p/16fc2a077bed505b04dc94c318cab182.jpg",
+		"price":1009000
+	}
+
+]
+
+
+
+
+export const getProducts = () => {
+    return books;
+    
+}
+
+
